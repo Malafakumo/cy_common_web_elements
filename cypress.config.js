e27e9f2fe10af8eec150caa20ba989cd24da0349
Malafakumo Akpos <Malafakumo.akpos@wemabank.com>
2025-02-04 09:34:34 +0100

    Added a table element test

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
 
-    // baseUrl: "https://rahulshettyacademy.com/AutomationPractice/"
+    baseUrl: "https://rahulshettyacademy.com/AutomationPractice/"
 
 
 
